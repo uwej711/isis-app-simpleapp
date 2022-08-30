70d13bc3e563ce942c4a2c40586df1d388ef0c10
Dan Haywood <dan@haywood-associates.co.uk>
2022-08-30 22:53:34 +0100

    no joy yet

--- webapp-tests/src/test/java/domainapp/webapp/bdd/specs/CucumberTestAbstract.java
+++ webapp-tests/src/test/java/domainapp/webapp/bdd/specs/CucumberTestAbstract.java
@@ -18,7 +18,30 @@
  */
 package domainapp.webapp.bdd.specs;
 
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.apache.isis.core.config.presets.IsisPresets;
+import org.apache.isis.core.runtimeservices.IsisModuleCoreRuntimeServices;
+import org.apache.isis.persistence.jpa.eclipselink.IsisModulePersistenceJpaEclipselink;
+import org.apache.isis.security.bypass.IsisModuleSecurityBypass;
+import org.apache.isis.testing.fixtures.applib.IsisModuleTestingFixturesApplib;
+import org.apache.isis.testing.integtestsupport.applib.IsisIntegrationTestAbstract;
+
+import domainapp.modules.simple.SimpleModule;
+import domainapp.webapp.application.ApplicationModule;
 import domainapp.webapp.integtests.WebAppIntegTestAbstract;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 
 /**
@@ -26,9 +49,63 @@ import io.cucumber.spring.CucumberContextConfiguration;
  * Provides the App's Spring Context for testing with Cucumber.
  *
  */
+@SpringBootTest(
+        classes = {
+                // we use a slightly different configuration compared to the production (AppManifest/webapp)
+                domainapp.webapp.integtests.WebAppIntegTestAbstract.TestApp.class,
+                BddStepDefsModule.class,
+                ApplicationModule.class,
+        }
+)
 @CucumberContextConfiguration
-public abstract class CucumberTestAbstract extends WebAppIntegTestAbstract {
+@ActiveProfiles("test")
+public abstract class CucumberTestAbstract extends IsisIntegrationTestAbstract {
 
-    // any cucumber specific stuff might go here
+    /**
+     * Compared to the production app manifest <code>domainapp.webapp.AppManifest</code>,
+     * here we in effect disable security checks, and we exclude any web/UI modules.
+     */
+    @SpringBootConfiguration
+    @EnableAutoConfiguration
+    @EnableJpaRepositories
+    @Import({
+
+            IsisModuleCoreRuntimeServices.class,
+            IsisModuleSecurityBypass.class,
+            IsisModulePersistenceJpaEclipselink.class,
+            IsisModuleTestingFixturesApplib.class,
+
+            SimpleModule.class
+    })
+    @PropertySources({
+            @PropertySource(IsisPresets.H2InMemory_withUniqueSchema),
+            @PropertySource(IsisPresets.UseLog4j2Test),
+    })
+    public static class TestApp {
+
+    }
+
+    private String today;
+    private String actualAnswer;
+
+//    @Given("today is Sunday")
+//    public void today_is_sunday() {
+//        this.today = "Sunday";
+//    }
+
+    @Given("^today is (.+)$")
+    public void today_is(final String today) {
+        this.today = today;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(final String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
 
 }
